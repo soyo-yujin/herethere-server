ea5bee84d2f2e10bbaf2c8b1d8c687bfcac7e45d
root <root@ip-172-31-36-66.ap-northeast-2.compute.internal>
2019-10-12 13:32:31 +0000

    환 구축

---
+++ pdos/IndexPdo.php
@@ -0,0 +1,118 @@
+<?php
+
+//READ
+function test()
+{
+    $pdo = pdoSqlConnect();
+    $query = "SELECT * from users;";
+
+    $st = $pdo->prepare($query);
+    //    $st->execute([$param,$param]);
+    $st->execute();
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st = null;
+    $pdo = null;
+
+    return $res;
+}
+
+//READ
+function testDetail($testNo)
+{
+    $pdo = pdoSqlConnect();
+    $query = "SELECT * FROM TEST_TB WHERE no = ?;";
+
+    $st = $pdo->prepare($query);
+    $st->execute([$testNo]);
+    //    $st->execute();
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st = null;
+    $pdo = null;
+
+    return $res[0];
+}
+
+
+function testPost($name)
+{
+    $pdo = pdoSqlConnect();
+    $query = "INSERT INTO TEST_TB (name) VALUES (?);";
+
+    $st = $pdo->prepare($query);
+    $st->execute([$name]);
+
+    $st = null;
+    $pdo = null;
+
+}
+
+// CREATE
+//    function addMaintenance($message){
+//        $pdo = pdoSqlConnect();
+//        $query = "INSERT INTO MAINTENANCE (MESSAGE) VALUES (?);";
+//
+//        $st = $pdo->prepare($query);
+//        $st->execute([$message]);
+//
+//        $st = null;
+//        $pdo = null;
+//
+//    }
+
+
+// UPDATE
+//    function updateMaintenanceStatus($message, $status, $no){
+//        $pdo = pdoSqlConnect();
+//        $query = "UPDATE MAINTENANCE
+//                        SET MESSAGE = ?,
+//                            STATUS  = ?
+//                        WHERE NO = ?";
+//
+//        $st = $pdo->prepare($query);
+//        $st->execute([$message, $status, $no]);
+//        $st = null;
+//        $pdo = null;
+//    }
+
+// RETURN BOOLEAN
+//    function isRedundantEmail($email){
+//        $pdo = pdoSqlConnect();
+//        $query = "SELECT EXISTS(SELECT * FROM USER_TB WHERE EMAIL= ?) AS exist;";
+//
+//
+//        $st = $pdo->prepare($query);
+//        //    $st->execute([$param,$param]);
+//        $st->execute([$email]);
+//        $st->setFetchMode(PDO::FETCH_ASSOC);
+//        $res = $st->fetchAll();
+//
+//        $st=null;$pdo = null;
+//
+//        return intval($res[0]["exist"]);
+//
+//    }
+
+
+function isValidJWToken($userid, $userpw)
+{
+
+    $pdo = pdoSqlConnect();
+//        echo "현재 로그인한 유저 아이디: $userid";
+//        echo "pw : $userpw";
+    $query = "SELECT EXISTS(SELECT * FROM guest WHERE userid = ? and userpw = ?) AS exist";
+//        echo $query;
+    $st = $pdo->prepare($query);
+    //    $st->execute([$param,$param]);
+    $st->execute([$userid, $userpw]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st=null;
+    $pdo = null;
+
+    return array("intval"=>intval($res[0]["exist"]), "userid"=>$userid);
+}
